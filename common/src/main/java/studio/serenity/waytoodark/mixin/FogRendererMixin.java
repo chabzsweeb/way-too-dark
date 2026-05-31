a8c19a27f8a166da0df028101313069dcb6c303b
chabzsweeb <3justaguyvr3@gmail.com>
2026-05-31 13:43:28 -0400

    Update FogRendererMixin.java

--- common/src/main/java/studio/serenity/waytoodark/mixin/FogRendererMixin.java
+++ common/src/main/java/studio/serenity/waytoodark/mixin/FogRendererMixin.java
@@ -1,7 +1,6 @@
 package studio.serenity.waytoodark.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.FogRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,14 +13,7 @@ import studio.serenity.waytoodark.SuffocationState;
 public class FogRendererMixin {
 
     @Inject(method = "method_24873", at = @At("RETURN"), remap = false)
-    private static void wayTooDark$overrideFog(
-            Camera camera,
-            Object fogMode,
-            float viewDistance,
-            int i,
-            int j,
-            int k,
-            CallbackInfoReturnable<Float> cir) {
+    private static void wayTooDark$overrideFog(CallbackInfoReturnable<Float> cir) {
         if (!SuffocationState.shouldApplyDarkness()) return;
         SuffocationState.setDynamicLightBoost(DynamicLightAdapter.getDynamicLightRadius());
         RenderSystem.setShaderFogStart(0.0F);
