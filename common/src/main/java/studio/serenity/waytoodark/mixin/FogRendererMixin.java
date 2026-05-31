e843cc7047b325b1a9fcf51cbd8217c3b06e9338
chabzsweeb <3justaguyvr3@gmail.com>
2026-05-31 14:03:41 -0400

    Update FogRendererMixin.java

--- common/src/main/java/studio/serenity/waytoodark/mixin/FogRendererMixin.java
+++ common/src/main/java/studio/serenity/waytoodark/mixin/FogRendererMixin.java
@@ -1,19 +1,18 @@
 package studio.serenity.waytoodark.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.renderer.FogRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import studio.serenity.waytoodark.DynamicLightAdapter;
 import studio.serenity.waytoodark.SuffocationState;
 
-@Mixin(FogRenderer.class)
+@Mixin(targets = "me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager", remap = false)
 public class FogRendererMixin {
 
-    @Inject(method = "method_24873", at = @At("RETURN"), remap = false)
-    private static void wayTooDark$overrideFog(CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "update", at = @At("RETURN"), remap = false)
+    private void wayTooDark$overrideFog(CallbackInfo ci) {
         if (!SuffocationState.shouldApplyDarkness()) return;
         SuffocationState.setDynamicLightBoost(DynamicLightAdapter.getDynamicLightRadius());
         RenderSystem.setShaderFogStart(0.0F);
